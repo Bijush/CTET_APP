44826ed8adea63ab275f6be74f3e30f387e57600
bijush <38435589+Bijush@users.noreply.github.com>
2026-07-10 22:49:49 +0530

    Add files via upload

---
+++ data/questionPaper/pdf.js
@@ -0,0 +1,11 @@
+const PAPERS = [
+
+{
+    title: "ADRE Grade III Question Paper 2024",
+    exam: "ADRE",
+    year: 2024,
+    subject: "General",
+    language: "English",
+    file: "pdfs/ADRE_2024.pdf"
+}
+];
